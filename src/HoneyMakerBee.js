d3304ab45876e86451a46b1471b3986c343684cf
Jawadban <Jawadbanga@gmail.com>
2016-12-19 15:19:13 -0800

    Completed the exercise

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,17 @@
 var HoneyMakerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'make honey';
+  this.honeyPot = 0;
 };
 
+HoneyMakerBee.prototype = Object.create(Grub.prototype);
+HoneyMakerBee.prototype.makeHoney = function(first_argument) {
+  this.honeyPot ++;
+};
+
+HoneyMakerBee.prototype.giveHoney = function(first_argument) {
+  this.honeyPot --;
+};
+
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
